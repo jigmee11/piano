de89a8cc654be17fb7b568e5a29fc6288a4a374b
jigmee11 <jigmee2006@gmail.com>
2020-06-19 14:10:40 +0800

    update

---
+++ public/firebase.js
@@ -0,0 +1,12 @@
+
+  var firebaseConfig = {
+    apiKey: "AIzaSyCfhGwIbFF5mOhl-znYPM-ogQiuYheuvLI",
+    authDomain: "piano-nest.firebaseapp.com",
+    databaseURL: "https://piano-nest.firebaseio.com",
+    projectId: "piano-nest",
+    storageBucket: "piano-nest.appspot.com",
+    messagingSenderId: "253345947268",
+    appId: "1:253345947268:web:502d419d107b249e4d6cd1"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
---
+++ public/login.js
@@ -0,0 +1,64 @@
+const db = firebase.database();
+const loginer = () => {
+    let a;
+    db.ref(`users/${ner.value}`).once('value')
+        .then(snapshot => {
+            if (snapshot.val().pass == pass.value) {}
+            if (snapshot.val().pass != pass.value) {
+                const d = document.createElement('h3');
+                d.innerHTML = 'username or password incorrect';
+                document.getElementsByTagName('div')[1].appendChild(d);
+            }
+        })
+}
+const register = () => {
+    db.ref(`users`).once('value')
+        .then(snapshot => {
+            if (ner.value == null) {
+                db.ref(`users/${ner.value}`).set({
+                    pass: pass.value
+                })
+            } else {
+                const d = document.createElement('h3');
+                d.innerHTML = 'already registreted';
+                document.getElementsByTagName('div')[1].appendChild(d);
+            }
+        })
+}
+const googler = () => {
+    var provider = new firebase.auth.GoogleAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
+
+const facer = () => {
+    var provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+    }).catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+    });
+}
